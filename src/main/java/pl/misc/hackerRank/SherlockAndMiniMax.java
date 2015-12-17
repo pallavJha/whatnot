b133001961cb64c921b030892f2ccfcad0b125f1
pallavJha <pallav_kumar71@yahoo.com>
2015-12-17 19:43:31 +0530

    Sherlock&MiniMax (Eclipse:Commit Office)

---
+++ src/main/java/pl/misc/hackerRank/SherlockAndMiniMax.java
@@ -0,0 +1,22 @@
+package pl.misc.hackerRank;
+
+import java.util.Scanner;
+
+public class SherlockAndMiniMax {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		int p = sc.nextInt();
+		int q = sc.nextInt();
+
+		
+	}
+}
