03a7060ee19d532246aa292b3ec90e7b8ce8ae8f
pallavJha <pallav_kumar71@yahoo.com>
2015-12-23 13:44:21 +0530

    Priyanka And Toys (Eclipse:Office)

---
+++ src/main/java/pl/misc/hackerRank/PriyankaAndToys.java
@@ -0,0 +1,32 @@
+package pl.misc.hackerRank;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PriyankaAndToys {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] w = new int[n];
+		for (int i = 0; i < n; i++) {
+			w[i] = sc.nextInt();
+		}
+		sc.close();
+
+		Arrays.sort(w);
+		
+		int money = 1;
+		int min = w[0];
+		
+		for (int i = 1; i < n; i++) {
+			if(w[i]-min >= 5){
+				money++;
+				min = w[i];
+			}
+		}
+		
+		System.out.println(money);
+	}
+}
