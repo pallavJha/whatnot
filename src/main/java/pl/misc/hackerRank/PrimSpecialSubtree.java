c56e0eeab7fb4a6d792808a3cfbbe637677c60eb
pallavJha <pallav_kumar71@yahoo.com>
2016-07-20 10:29:19 +0530

    Prim's Algorithm

--- src/main/java/pl/misc/hackerRank/PrimSpecialSubtree.java
+++ src/main/java/pl/misc/hackerRank/PrimSpecialSubtree.java
@@ -29,6 +29,11 @@ public class PrimSpecialSubtree {
         int noOfNodes = sc.nextInt();
         int noOfEdges = sc.nextInt();
         int[][] adjMatrix = new int[noOfNodes + 1][noOfNodes + 1];
+        for (int i = 0; i < noOfNodes + 1; i++) {
+            for (int j = 0; j < noOfNodes + 1; j++) {
+                adjMatrix[i][j] = -1;
+            }
+        }
         for (int i = 0; i < noOfEdges; i++) {
             int row = sc.nextInt();
             int col = sc.nextInt();
@@ -44,10 +49,10 @@ public class PrimSpecialSubtree {
 
     private static int primAlgorithm(int noOfNodes, int[][] adjMatrix, int startNode) {
         int minimumDistance = 0;
-        int[] parent = new int[noOfNodes+1];
-        int[] distance = new int[noOfNodes+1];
+        int[] parent = new int[noOfNodes + 1];
+        int[] distance = new int[noOfNodes + 1];
         int total = 1;
-        boolean[] visited = new boolean[noOfNodes+1];
+        boolean[] visited = new boolean[noOfNodes + 1];
         parent[startNode] = 0;
         visited[startNode] = true;
         for (int i = 1; i <= noOfNodes; i++) {
@@ -57,23 +62,25 @@ public class PrimSpecialSubtree {
         int currentNode = startNode;
         while (total < noOfNodes) {
             for (int i = 1; i <= noOfNodes; i++) {
-                if (!visited[i] && adjMatrix[currentNode][i] != 0 && adjMatrix[currentNode][i] < distance[i]) {
+                if (!visited[i] && adjMatrix[currentNode][i] != -1 && adjMatrix[currentNode][i] < distance[i]) {
                     distance[i] = adjMatrix[currentNode][i];
                     parent[i] = currentNode;
                 }
             }
 
             int min = Integer.MAX_VALUE;
+            int nextNode = currentNode;
             for (int i = 1; i <= noOfNodes; i++) {
                 if (!visited[i] && distance[i] < min) {
                     min = distance[i];
-                    currentNode = i;
+                    nextNode = i;
                 }
             }
 
             visited[currentNode] = true;
             minimumDistance += min;
             total++;
+            currentNode = nextNode;
         }
         return minimumDistance;
     }
