5d3447ffd40823bbee2009fec7440242b7549699
pallavJha <pallav_kumar71@yahoo.com>
2016-07-24 20:05:56 +0530

    City Of Blinding Lights Floyd Warshall

--- src/main/java/pl/misc/hackerank/graph/CityOfBlindingLightsFloydWarshall.java
+++ src/main/java/pl/misc/hackerank/graph/CityOfBlindingLightsFloydWarshall.java
@@ -1,6 +1,7 @@
 package pl.misc.hackerank.graph;
 
 import java.util.*;
+import java.util.stream.IntStream;
 
 /**
  * @author pallav
@@ -22,7 +23,7 @@ public class CityOfBlindingLightsFloydWarshall {
         Scanner scanner = new Scanner(System.in);
         int noOfNodes = scanner.nextInt();
         int noOfEdges = scanner.nextInt();
-        int[][] adjMatrix = new int[noOfNodes+1][noOfNodes+1];
+        int[][] adjMatrix = prepareAdjMatrix(noOfNodes);
         for (int i = 0; i < noOfEdges; i++) {
             int row = scanner.nextInt();
             int col = scanner.nextInt();
@@ -30,16 +31,42 @@ public class CityOfBlindingLightsFloydWarshall {
             adjMatrix[row][col] = weight;
         }
         int noOfTestCases = scanner.nextInt();
-        List<Integer> output = new LinkedList<>();
+
+        adjMatrix = calculateShortestDistance(noOfNodes, adjMatrix);
+
+        //print result list
         for (int i = 0; i < noOfTestCases; i++) {
-            output.add(calculateShortestDistance(scanner.nextInt(), scanner.nextInt(), noOfNodes, adjMatrix));
+            int output = adjMatrix[scanner.nextInt()][scanner.nextInt()];
+            System.out.println(output == 9999 ? -1 : output);
         }
 
-        //print resultList
-        output.forEach(System.out::println);
     }
 
-    private static int calculateShortestDistance(int startNode, int endNode, int nofNodes, int[][] adjMatrix) {
-        return 0;
+    private static int[][] calculateShortestDistance(int nofNodes, int[][] adjMatrix) {
+        for (int i = 1; i <= nofNodes; i++) {
+            for (int j = 1; j <= nofNodes; j++) {
+                for (int k = 1; k <= nofNodes; k++) {
+                    if ((adjMatrix[j][i] + adjMatrix[i][k]) < adjMatrix[j][k]) {
+                        adjMatrix[j][k] = adjMatrix[j][i] + adjMatrix[i][k];
+                    }
+                }
+            }
+        }
+
+        //Diagonal
+        for (int i = 0; i <= nofNodes; i++) {
+            adjMatrix[i][i] = 0;
+        }
+
+        return adjMatrix;
+    }
+
+    private static int[][] prepareAdjMatrix(int noOfNodes) {
+        int[][] adjMatrix = new int[noOfNodes + 1][noOfNodes + 1];
+        for (int[] arr : adjMatrix) {
+            IntStream.rangeClosed(0, noOfNodes)
+                    .forEach(i -> arr[i] = 9999);
+        }
+        return adjMatrix;
     }
 }
