a6692c3de81fe820a89dfc6d050fd1893db7bf70
pallavJha <pallav_kumar71@yahoo.com>
2017-04-06 01:35:09 +0530

    Added code for GoodLandElectricity

--- src/main/java/pl/misc/hackerank/greedy/GoodlandElectricity.java
+++ src/main/java/pl/misc/hackerank/greedy/GoodlandElectricity.java
@@ -18,87 +18,52 @@ public class GoodlandElectricity {
 		for (int i = 0; i < n; i++) {
 			arr[i] = sc.nextInt();
 		}
-		Map<Integer, Integer> adjMatrix;
+		// Map<Integer, Integer> adjMatrix;
 		try {
-			adjMatrix = getAdjacencyList(arr, n, k);
-			System.out.println(adjMatrix);
-			System.out.println(getNoOfTowers(adjMatrix));
+			int towersCount = getTowersCount(arr, n, k);
+			System.out.println(towersCount);
+			// System.out.println(getNoOfTowers(adjMatrix));
 		} catch (IOException e) {
 			System.out.println(-1);
 		}
 		sc.close();
 	}
 
-	private static Map<Integer, Integer> getAdjacencyList(int[] arr, int n, int k) throws IOException {
+	// private static Map<Integer, Integer> getAdjacencyList(int[] arr, int n,
+	// int k) throws IOException {
+	private static int getTowersCount(int[] arr, int n, int k) throws IOException {
 		Map<Integer, Integer> map = new HashMap<>();
+		Set<Integer> usedTowers = new HashSet<>();
 		for (int i = 0; i < n; i++) {
-			Set<Integer> adjacencySet = null;
-			boolean forwardLoopRequired = true;
-			if (i <= k) {
-				adjacencySet = new HashSet<>(2 * k - 1);
-				Integer prevTowerLocation = map.get(i - 1);
-				prevTowerLocation = prevTowerLocation == null ? -1 : prevTowerLocation;
+			int max = -1;
+			Set<Integer> adjacencySet = new HashSet<>(2 * k - 1);
+			Integer prevTowerLocation = map.get(i - 1);
+			prevTowerLocation = prevTowerLocation == null ? -1 : prevTowerLocation;
+			int distanceWithPrevTower = Math.abs(i - prevTowerLocation);
+			if (prevTowerLocation != -1 && distanceWithPrevTower < k) {
+				max = prevTowerLocation;
+			} else {
+				for (int j = i; j < i + k && j < n; j++) {
+					if (arr[j] > 0) {
+						adjacencySet.add(j);
+					}
+				}
 				for (int j = i; j > -1 && j > i - k; j--) {
 					if (arr[j] > 0) {
 						adjacencySet.add(j);
-						if (prevTowerLocation.equals(j)) {
-							adjacencySet = new HashSet<>();
-							adjacencySet.add(j);
-							forwardLoopRequired = false;
-							break;
-						}
 					}
-				}
-				if (forwardLoopRequired) {
-					for (int j = i; j < i + k && j < n; j++) {
-						if (arr[j] > 0) {
-							adjacencySet.add(j);
-							if (prevTowerLocation.equals(j)) {
-								adjacencySet = new HashSet<>();
-								adjacencySet.add(j);
-								break;
-							}
-						}
-					}
-				}
-			} else {
-				adjacencySet = new HashSet<>();
-				adjacencySet.add(map.get(i - 1));
-				adjacencySet.remove(i - k);
-				if ((i + k - 1) < arr.length && arr[i + k - 1] > 0) {
-					adjacencySet.add(i + k - 1);
 				}
 			}
-			if (adjacencySet.isEmpty()) {
+			if (adjacencySet.isEmpty() && max == -1) {
 				throw new IOException();
 			}
-			int max = 0;
-			if (adjacencySet.size() > 1) {
+			if (max == -1) {
 				max = Collections.max(adjacencySet);
 			}
-			else {
-				max = adjacencySet.iterator().next();
-			}
 			map.put(i, max);
+			usedTowers.add(max);
+			max = 0;
 		}
-		return map;
-	}
-
-	private static int getNoOfTowers(Map<Integer, Integer> adjMatrix) {
-		Map<Integer, Integer> cityToTower = new HashMap<>();
-		Set<Integer> usedTowers = new HashSet<>(adjMatrix.size());
-		adjMatrix.keySet().stream().forEach(t -> {
-			Integer connectedTowers = adjMatrix.get(t);
-			Integer previousTower = cityToTower.get(t - 1);
-			if (previousTower != null && connectedTowers.equals(previousTower)) {
-				cityToTower.put(t, previousTower);
-				usedTowers.add(previousTower);
-			} else {
-				int towerVal = connectedTowers;
-				cityToTower.put(t, towerVal);
-				usedTowers.add(towerVal);
-			}
-		});
 		return usedTowers.size();
 	}
 }
