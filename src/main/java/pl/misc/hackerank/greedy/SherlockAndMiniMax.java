e0db2e981150edc65de9eb4783be72e575780ac1
pallavJha <pallav_kumar71@yahoo.com>
2017-04-07 01:48:07 +0530

    Added code for Sherlock&MiniMax

--- src/main/java/pl/misc/hackerank/greedy/SherlockAndMiniMax.java
+++ src/main/java/pl/misc/hackerank/greedy/SherlockAndMiniMax.java
@@ -1,6 +1,5 @@
-package pl.misc.hackerank.unmanaged;
+package pl.misc.hackerank.greedy;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -9,21 +8,6 @@ import java.util.Scanner;
 public class SherlockAndMiniMax {
 
 	public static HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-
-	public static void main2(String... strings) {
-		long start = System.currentTimeMillis();
-
-		for (int i = 70283784; i <= 302962359; i++) {
-			for (int j = 0; j < 73; j++) {
-
-			}
-		}
-		long end = System.currentTimeMillis();
-		System.out.println(end - start);
-
-		int i = 55;
-		System.out.println(~i);
-	}
 
 	public static void main(String[] args) throws InterruptedException {
 
@@ -40,37 +24,18 @@ public class SherlockAndMiniMax {
 		int q = sc.nextInt();
 
 		sc.close();
-
+		
 		int temp = p;
-		
-		Arrays.sort(arr);
-		
-		for (int i = 0; i < n; i++) {
-			temp_arr[i] = Math.abs(arr[i] - temp);
-		}
-		
-		int min = findMinimum(temp_arr);
-		if (min != 0) {
-			map.put(temp, min);
-		}
-		System.out.println(temp);
-		printArray(temp_arr);
-		System.out.println(min);
-		temp++;
-		
-		
+
 		while (temp <= q) {
+
 			for (int i = 0; i < n; i++) {
-				temp_arr[i] = Math.abs(temp_arr[i] - 1);
+				temp_arr[i] = Math.abs(arr[i] - temp);
 			}
-			
-			min = findMinimum(temp_arr);
+			int min = findMinimum(temp_arr);
 			if (min != 0) {
 				map.put(temp, min);
 			}
-			System.out.println(temp);
-			printArray(temp_arr);
-			System.out.println(min);
 			temp++;
 		}
 
@@ -102,12 +67,5 @@ public class SherlockAndMiniMax {
 				break;
 			}
 		}
-	}
-	
-	public static void printArray(int[] arr){
-		for(int i = 0 ; i < arr.length;i++){
-			System.out.print(arr[i]+" ");
-		}
-		System.out.println();
 	}
 }
