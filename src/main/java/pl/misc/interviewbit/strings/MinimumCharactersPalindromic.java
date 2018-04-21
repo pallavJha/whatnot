905f9ededc9365600e7f90757a8cefb0bfc4f25e
pallavJha <pallav_kumar71@yahoo.com>
2018-04-21 19:30:59 +0530

    interviewbit

--- src/main/java/pl/misc/interviewbit/strings/MinimumCharactersPalindromic.java
+++ src/main/java/pl/misc/interviewbit/strings/MinimumCharactersPalindromic.java
@@ -7,26 +7,41 @@ package pl.misc.interviewbit.strings;
 public class MinimumCharactersPalindromic {
 
     public int minChars(String A) {
-        int additions = 0;
-        for (int i = 0, j = A.length() - 1; i < j; j--) {
-            System.out.println(i + " " + A.charAt(i) + ", " + j + " " + A.charAt(j));
-            if (A.charAt(i) == A.charAt(j)) {
+        String aWithItsReverse = A + "$" + new StringBuilder(A).reverse().toString();
+        int[] lps = prepareLPSArray(aWithItsReverse);
+        return A.length() - lps[lps.length - 1];
+    }
+
+
+    private int[] prepareLPSArray(String A) {
+        int[] lps = new int[A.length()];
+        int i = 1;
+        int len = 0;
+        lps[0] = 0;
+
+        while (i < A.length()) {
+            if (A.charAt(len) == A.charAt(i)) {
+                len++;
+                lps[i] = len;
                 i++;
-                System.out.println("Already Present");
             } else {
-                additions++;
-                System.out.println("Added");
+                if (len > 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
             }
         }
-        return additions;
+        return lps;
     }
 
     public static void main(String[] args) {
         MinimumCharactersPalindromic obj = new MinimumCharactersPalindromic();
-        //System.out.println(obj.minChars("ABC"));
-        //System.out.println(obj.minChars("BAACECAAAAA"));
-        //System.out.println(obj.minChars("ABBCD"));//DCBBABBCD
+        System.out.println(obj.minChars("ABC"));
+        System.out.println(obj.minChars("BAACECAAAAA"));
+        System.out.println(obj.minChars("ABBCD"));//DCBBABBCD
         System.out.println(obj.minChars("ABEACBBC"));
-        //System.out.println(obj.minChars("HQGHUMEAYLNLFDXFI"));//HQGHUMEAYLNLFDXFI
+        System.out.println(obj.minChars("HQGHUMEAYLNLFDXFI"));//HQGHUMEAYLNLFDXFI
     }
 }
