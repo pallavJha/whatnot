c2f85b924728faa8d79ca55bd0ea6439baf10ac8
pallavJha <pallav_kumar71@yahoo.com>
2018-05-27 22:11:26 +0530

    design patterns

---
+++ src/main/java/pl/misc/geeksforgeeks/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Solution for questions present in <a href="https://www.geeksforgeeks.org/must-coding-questions-company-wise/">Must Coding Questions</a>. 
+ * 
+ * These classes contain the solution to the questions present in above link.
+ *
+ * @since 1.0
+ * @author Pallav Jha
+ * @version 1.1
+ */
+package pl.misc.geeksforgeeks;
