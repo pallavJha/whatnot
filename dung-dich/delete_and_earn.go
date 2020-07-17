47a7e9070c4fa09ac8de1f92ae763ba63d8b068a
pallav.jha <pallav.jha@soroco.com>
2020-07-17 20:33:25 +0530

    add inprogress items

---
+++ dung-dich/delete_and_earn.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// TODO: In Progress
+
+func main() {
+	fmt.Println(deleteAndEarn([]int{3, 4, 2}))
+	fmt.Println(deleteAndEarn([]int{2, 2, 3, 3, 3, 4}))
+	fmt.Println(deleteAndEarn([]int{3, 2, 5, 7, 5, 6}))
+	fmt.Println(deleteAndEarn([]int{1, 1, 1, 2, 4, 5, 5, 5, 6}))
+	fmt.Println(deleteAndEarn([]int{8, 7, 3, 8, 1, 4, 10, 10, 10, 2}))
+}
+
+func deleteAndEarn(nums []int) int {
+	return 0
+}
---
+++ dung-dich/longest_string_chain.go
@@ -0,0 +1,21 @@
+package main
+
+func main() {
+
+	// var str1Bit, str2Bit uint64
+	//
+	// str1 := "abc"
+	// str2 := "abcd"
+	//
+	// for _, r := range str1 {
+	// 	str1Bit |= uint64(r)
+	// }
+	//
+	// for _, r := range str2 {
+	// 	str1Bit |= uint64(r)
+	// }
+	//
+
+
+
+}
