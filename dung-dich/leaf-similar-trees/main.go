eea45f3114175ed59aa9323cb84b5069fc991c20
Pallav Jha <pallav_kumar71@yahoo.com>
2020-08-30 14:49:28 +0530

    Update main.go

--- dung-dich/leaf-similar-trees/main.go
+++ dung-dich/leaf-similar-trees/main.go
@@ -6,30 +6,36 @@ func main() {
 	
 }
 
- type TreeNode struct {
+type TreeNode struct {
      Val int
      Left *TreeNode
      Right *TreeNode
  }
 
-var leafs []int
+var leaves []int
 var index int
 
 func leafSimilar(root1 *TreeNode, root2 *TreeNode) bool {
-	leafs = []int{}
+	leaves = []int{}
 	index = 0
+    // false is returned if both the tress are not non nil
 	if !(root1 != nil && root2 != nil) {
 		return false
 	}
+    // preOrder fills up the leaves list after traversing 
+    // the first tree
 	preOrder1(root1)
-	fmt.Println(leafs)
+	// fmt.Println(leafs)
 	return preOrder2(root2)
 }
 
+// addLeaf appends the passed value into the leaves list
 func addLeaf(value int) {
-	leafs = append(leafs, value)
+	leaves = append(leaves, value)
 }
 
+// checkLeaf checks whether the element at the next index 
+// matches with the passed value
 func checkLeaf(value int) bool {
 	index++
 	if index == len(leafs) + 1{
@@ -38,6 +44,8 @@ func checkLeaf(value int) bool {
 	return leafs[index-1] == value
 }
 
+// preOrder1 traverses over the first tree
+// and fills up the leaves list
 func preOrder1(node *TreeNode) {
 	if node.Left != nil {
 		preOrder1(node.Left)
@@ -50,6 +58,9 @@ func preOrder1(node *TreeNode) {
 	}
 }
 
+// preOrder2 traverses over the second tree
+// and checks if the leaf node values are present
+// in the leaves list
 func preOrder2(node *TreeNode) bool {
 	if node.Left != nil {
 		equal := preOrder2(node.Left)
