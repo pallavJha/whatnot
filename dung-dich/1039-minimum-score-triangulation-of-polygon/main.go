83a1025ec03183811960f2e83fb48b0bcb21f570
pallav.jha <pallav.jha@soroco.com>
2020-10-31 22:23:32 +0530

    add inputs and output in the comments

--- dung-dich/1039-minimum-score-triangulation-of-polygon/main.go
+++ dung-dich/1039-minimum-score-triangulation-of-polygon/main.go
@@ -6,13 +6,6 @@ import (
 )
 
 func main() {
-	// fmt.Println(minScoreTriangulation([]int{1, 2, 3}))
-	// fmt.Println(minScoreTriangulation([]int{3, 7, 4, 5}))
-	fmt.Println(minScoreTriangulation([]int{1, 2, 3, 4, 5}))
-	fmt.Println(minScoreTriangulation([]int{1, 2, 3, 4, 5, 6}))
-	fmt.Println(minScoreTriangulation([]int{1, 2, 3, 4, 5, 6, 7}))
-	// fmt.Println(minScoreTriangulation([]int{1, 3, 1, 4, 1, 5}))
-	// fmt.Println(minScoreTriangulation([]int{3, 5, 2, 5, 2, 6}))
 	// f, err := os.Create("./cpu_profile_2")
 	// if err != nil {
 	// 	log.Fatal(err)
@@ -22,12 +15,26 @@ func main() {
 	// 	log.Fatal(err)
 	// }
 	// defer pprof.StopCPUProfile()
+	// 6
+	fmt.Println(minScoreTriangulation([]int{1, 2, 3}))
+	// 144
+	fmt.Println(minScoreTriangulation([]int{3, 7, 4, 5}))
+	// 38
+	fmt.Println(minScoreTriangulation([]int{1, 2, 3, 4, 5}))
+	// 68
+	fmt.Println(minScoreTriangulation([]int{1, 2, 3, 4, 5, 6}))
+	// 110
+	fmt.Println(minScoreTriangulation([]int{1, 2, 3, 4, 5, 6, 7}))
+	// 13
+	fmt.Println(minScoreTriangulation([]int{1, 3, 1, 4, 1, 5}))
+	// 98
+	fmt.Println(minScoreTriangulation([]int{3, 5, 2, 5, 2, 6}))
 	// 140295
-	// fmt.Println(minScoreTriangulation([]int{35, 73, 90, 27, 71, 80, 21, 33, 33, 13, 48, 12, 68, 70, 80, 36, 66, 3, 70, 58}))
+	fmt.Println(minScoreTriangulation([]int{35, 73, 90, 27, 71, 80, 21, 33, 33, 13, 48, 12, 68, 70, 80, 36, 66, 3, 70, 58}))
 	// 153657
-	// fmt.Println(minScoreTriangulation([]int{38, 76, 69, 32, 24, 35, 82, 30, 86, 77, 92, 3, 35, 20, 84, 67, 23, 58, 94, 10}))
+	fmt.Println(minScoreTriangulation([]int{38, 76, 69, 32, 24, 35, 82, 30, 86, 77, 92, 3, 35, 20, 84, 67, 23, 58, 94, 10}))
 	// 103598
-	// fmt.Println(minScoreTriangulation([]int{5, 80, 62, 45, 96, 100, 17, 72, 67, 64, 20, 66, 41, 68, 34, 67, 35, 24, 76, 2}))
+	fmt.Println(minScoreTriangulation([]int{5, 80, 62, 45, 96, 100, 17, 72, 67, 64, 20, 66, 41, 68, 34, 67, 35, 24, 76, 2}))
 }
 
 type Node struct {
@@ -68,7 +75,7 @@ func createHash(start *Node) uint64 {
 	var hash uint64
 	temp := start
 	for temp != nil {
-		hash |= (1 << temp.Index)
+		hash |= 1 << temp.Index
 		if temp.Next == start {
 			break
 		}
